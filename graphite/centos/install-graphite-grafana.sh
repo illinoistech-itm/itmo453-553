1b45a6e41720e6d32deb0357f5768097bc3e4db5
Jeremy Hajek <hajek@iit.edu>
2017-03-27 21:45:05 -0500

    Added command to start grafana-server

--- graphite/centos/install-graphite-grafana.sh
+++ graphite/centos/install-graphite-grafana.sh
@@ -90,3 +90,6 @@ sudo cp -v graphite-api.service /lib/systemd/system/graphite-api.service
 # Listing 4.60: Enabling and starting the systemd Graphite-API daemons
 sudo systemctl enable graphite-api.service
 sudo systemctl start graphite-api.service
+
+# Starting Grafana
+sudo service grafana-server start
