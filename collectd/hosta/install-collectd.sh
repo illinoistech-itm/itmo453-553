0f1675bb3a3e6e4fe5178957946ff6a847913b4d
Jeremy Hajek <hajek@iit.edu>
2017-04-04 21:41:49 -0500

    updated hosta install script

--- collectd/hosta/install-collectd.sh
+++ collectd/hosta/install-collectd.sh
@@ -6,5 +6,11 @@ sudo sudo add-apt-repository ppa:collectd/collectd-5.5
 sudo apt-get update
 sudo apt-get -y install collectd
 
+#copy the provided collectd.conf file overwritting the default collectd.conf
+sudo mv ./collectd.conf /etc
+
+#Copying the provided and configured collectd.d default plugins to be loaded
+sudo mv ./collectd.d /etc/
+
 sudo update-rc.d collectd defaults
 sudo service collectd start
